67320da0298cee37c8188282917c74c1235d726d
run <a936381813@163.com>
2020-09-03 11:39:19 +0800

    init project

---
+++ SwiftUIDemo/SwiftUIDemo/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SwiftUIDemo
+//
+//  Created by dev-yxr on 2020/9/3.
+//  Copyright © 2020 dev-yxr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
