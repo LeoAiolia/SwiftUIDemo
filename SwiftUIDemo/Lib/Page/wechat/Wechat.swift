bf4c18a7d2d6f8d1241155b7d89e7604b9fd1d70
run <a936381813@163.com>
2023-08-29 17:41:21 +0800

    feat: add wechat page

---
+++ SwiftUIDemo/Lib/Page/wechat/Wechat.swift
@@ -0,0 +1,21 @@
+//
+//  Wechat.swift
+//  SwiftUIDemo
+//
+//  Created by run on 2023/8/29.
+//  Copyright © 2023 dev-yxr. All rights reserved.
+//
+
+import SwiftUI
+
+struct Wechat: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Wechat_Previews: PreviewProvider {
+    static var previews: some View {
+        Wechat()
+    }
+}
--- SwiftUIDemo/ViewController.swift
+++ SwiftUIDemo/ViewController.swift
@@ -23,11 +23,25 @@ class ViewController: UIViewController {
         button.frame = CGRect(x: 100, y: 300, width: 200, height: 50)
         button.addTarget(self, action: #selector(touchBtn(sender:)), for: .touchUpInside)
         view.addSubview(button)
+        
+        let button2 = UIButton(type: .system)
+        button2.setTitle("wechat", for: .normal)
+        button2.setTitleColor(.red, for: .normal)
+        button2.titleLabel?.font = .systemFont(ofSize: 18)
+        button2.frame = CGRect(x: 100, y: 360, width: 200, height: 50)
+        button2.addTarget(self, action: #selector(touchBtn2(sender:)), for: .touchUpInside)
+        view.addSubview(button2)
     }
     
     @objc func touchBtn(sender: UIButton) {
         
         let vc = UIHostingController(rootView: ContentView().environmentObject(UserData()))
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @objc func touchBtn2(sender: UIButton) {
+        
+        let vc = UIHostingController(rootView: Wechat().environmentObject(UserData()))
         self.navigationController?.pushViewController(vc, animated: true)
     }
 }
